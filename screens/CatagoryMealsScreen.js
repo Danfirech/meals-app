e7f561a2cdd2b8f5971534136fe717b7613567e7
Dan Possehl <danielbpossehl@gmail.com>
2021-12-20 10:23:58 -0700

    isntalled fonts

--- screens/CatagoryMealsScreen.js
+++ screens/CatagoryMealsScreen.js
@@ -4,7 +4,7 @@ import { View, Text, StyleSheet } from "react-native";
 const CategoryMealsScreen = (props) => {
   return (
     <View style={styles.screen}>
-      <Text>The Category Meals Screen Screen!</Text>
+      <Text>The Category Meals Screen!</Text>
     </View>
   );
 };
